292f34ccc7b89a0058bff3825478412ed598fcea
Thu Juli <thujuli037@gmail.com>
2022-03-14 20:34:34 +0800

    initial commit

---
+++ 06 Create Social Login/config/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+# Database Configuration
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Override Installed Apps
+INSTALLED_APPS += [
+    'blog.apps.BlogConfig'
+]
