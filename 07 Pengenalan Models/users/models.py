c7213a342d983882a919c3120285c0f26f9c8eb2
Thu Juli <thujuli037@gmail.com>
2022-03-16 01:28:52 +0800

    setup models & manager

--- 07 Pengenalan Models/users/models.py
+++ 07 Pengenalan Models/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(auto_now=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    # Settings Field
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIERED_FIELDS = []
+
+    class Meta:
+        abstract = False
